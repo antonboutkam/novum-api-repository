808c5369d00a806a990b508ba9aaeac2c3500921
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-10 05:25:10 +0100

    removed debuuging line

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,5 @@ $aParts = array_reverse(explode('.', $_SERVER['HTTP_HOST']));
 
 $sPath = dirname($_SERVER['SCRIPT_FILENAME'], 3);
 
-echo  $sPath . '/index.php' . PHP_EOL;
 
 require_once $sPath . '/index.php';
